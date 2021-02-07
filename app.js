fcf3a411b57fc72c8e1fdbb2387835d6fdf81195
jakub-malycha <jakub.malycha1@gmail.com>
2021-02-07 15:02:06 +0100

    update for heroku

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const functions = require("./functions");
-const port = 3000;
+const port = process.env.PORT || 3000;
 const app = express();
 const path = require("path");
 
